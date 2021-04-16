9e1b40dcd74491463e008374eaf2303a31d07589
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-04-16 09:26:51 +0200

    changed styling and refresh Button

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,19 @@ export default class App extends Component {
   }
 
   componentDidMount(){
-    fetchAPI().then(e => this.setState({apiText:e.text}))
+   this.getAPI();
     
   }
+
+  getAPI = () =>{
+    fetchAPI().then(e => this.setState({apiText:e.text}))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>{this.state.apiText}</h1>
+        <button onClick={this.getAPI}>Neue Spruch</button>
       </div>
     )
   }
